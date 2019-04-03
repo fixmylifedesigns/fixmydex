0ae8faa40a076749ee33631a3d3c0416b96bef02
Irving Duran <ijd.irving@gmail.com>
2019-04-03 11:58:12 -0300

    home page

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
 import { Route, Switch, Link } from "react-router-dom";
-import styled from "styled-components"
+import styled from "styled-components";
 import Gen1 from "../Gen/Gen1";
+import fmlLogo from "../Images/fmlpokeball.png";
 
 const Home = styled.div`
-background:red;
-/* height:100vh; */
-`
+  background: red;
+  /* height:100vh; */
+  display:flex;
+  flex-direction: column;
+`;
+const FMLLogo = styled.img`
+  width: 500px;
+  margin: 20px auto;
+`;
 export default class HomePage extends Component {
   render() {
     return (
       <Home>
-        
-        <Link exact to="/">
-          <button>Home</button>
-        </Link>
         <Switch>
-        <Link exact to="/gen1">
-          <button>Gen1</button>
-        </Link>
-        <Link exact to="/">
-          <button>Home</button>
-        </Link>
-</Switch>
-
-
-        
+          <Link exact to="/">
+            <button>Home</button>
+          </Link>
+        </Switch>
+        <Switch>
+          <Link exact to="/gen1">
+            <button>Gen1</button>
+          </Link>
+        </Switch>
+        <FMLLogo src={fmlLogo} alt="" />
       </Home>
     );
   }
